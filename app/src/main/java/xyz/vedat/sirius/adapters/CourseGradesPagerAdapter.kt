93588ebc197314f85d3d05dadf4543f9d394c8f4
Ved <dev@vedat.xyz>
2022-11-11 17:30:54 +0300

    Abstract away the data list in `RefreshablePagerAdapter`

--- app/src/main/java/xyz/vedat/sirius/adapters/CourseGradesPagerAdapter.kt
+++ app/src/main/java/xyz/vedat/sirius/adapters/CourseGradesPagerAdapter.kt
@@ -5,15 +5,7 @@ import srs.data.CourseGrades
 import xyz.vedat.sirius.fragments.authenticated.components.CourseGradesFragment
 
 class CourseGradesPagerAdapter(fragment: Fragment) : RefreshablePagerAdapter<CourseGrades>(fragment) {
-    private var grades: List<CourseGrades>? = null
-
-    override fun onSubmittingList(newList: List<CourseGrades>) {
-        grades = newList
-    }
-
-    override fun getItemCount() = grades?.size ?: 0
-
     override fun createFragment(position: Int): Fragment {
-        return CourseGradesFragment.newInstance(grades!![position])
+        return CourseGradesFragment.newInstance(dataList!![position])
     }
 }
--- app/src/main/java/xyz/vedat/sirius/adapters/RefreshablePagerAdapter.kt
+++ app/src/main/java/xyz/vedat/sirius/adapters/RefreshablePagerAdapter.kt
@@ -8,18 +8,22 @@ abstract class RefreshablePagerAdapter<T>(fragment: Fragment) : FragmentStateAda
     private var idCounter = 0L
     private var itemIds: List<Long> = emptyList()
 
+    protected var dataList: List<T>? = null
+
     override fun getItemId(position: Int): Long = itemIds[position]
     override fun containsItem(itemId: Long): Boolean = itemIds.contains(itemId)
 
     @SuppressLint("NotifyDataSetChanged")
     fun submitList(newList: List<T>) {
-        onSubmittingList(newList)
-        itemIds = newList.indices.map { idCounter++ }
+        val finalList = onSubmittingList(newList)
+        dataList = finalList
+        itemIds = finalList.indices.map { idCounter++ }
         notifyDataSetChanged()
     }
 
-    protected abstract fun onSubmittingList(newList: List<T>)
+    protected open fun onSubmittingList(newList: List<T>) = newList
 
-    abstract override fun getItemCount(): Int
+    override fun getItemCount(): Int = dataList?.size ?: 0
+
     abstract override fun createFragment(position: Int): Fragment
 }
